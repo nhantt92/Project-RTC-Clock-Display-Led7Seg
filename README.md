d70737c42ee2ad91f2976d341f33029646597b7d
Nhan Truong <nhantt@icviet.com>
2018-03-17 15:45:33 +0700

    complete

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ Project-RTC-Clock-Display-Led7Seg
 ![Image Top](/assets/RTC-Clock-Display-Led-7Seg-bot.png)
 
 ## Firmware
+RTC_FW
+
+## Product
+
+![Image Top](/assets/image1.jpg)
+
+![Image Top](/assets/image2.jpg)
+
+![Image Top](/assets/image3.jpg)
--- RTC_FW/User/src/system.c
+++ RTC_FW/User/src/system.c
@@ -13,11 +13,11 @@ void System_Init(void)
   GPIO_WriteLow(RING_PORT, RING_PIN);
   sys.mode = MODE_NORMAL;
   sys.flag_pm = FALSE;
-  DS1307_SetPM(sys.flag_pm);
+//  DS1307_SetPM(sys.flag_pm);
   TIMER_InitTime(&sys.tick);
 }
 
-uint8_t check24h()
+uint8_t checkRing()
 {
   if(timenow.hh == 23 || timenow.hh == 0 || timenow.hh == 1 || timenow.hh == 2 || timenow.hh == 3 || timenow.hh == 4 || timenow.hh ==5)
     return 0;
@@ -25,25 +25,20 @@ uint8_t check24h()
     return 1;
 }
 
-uint8_t check12h()
-{
-  if(timenow.hh == 11 || timenow.hh == 12 || timenow.hh == 1 || timenow.hh == 2 || timenow.hh == 3 || timenow.hh == 4 || timenow.hh ==5)
-    return 0;
-  else
-    return 1;
-}
-
 void system_ring()
 {
-  if((sys.flag_pm == 0) && check24h() == 1)
+  if(checkRing() == 1)
   {
     if(timenow.mn == 0)
     {
-    if(timenow.ss < 4)
-    {
-      if(timenow.ss % 2 == 0)
+      if(timenow.ss < 4)
       {
-        GPIO_WriteHigh(RING_PORT, RING_PIN);
+        if(timenow.ss % 2 == 0)
+        {
+          GPIO_WriteHigh(RING_PORT, RING_PIN);
+        }
+        else
+          GPIO_WriteLow(RING_PORT, RING_PIN);
       }
       else
         GPIO_WriteLow(RING_PORT, RING_PIN);
@@ -51,31 +46,12 @@ void system_ring()
     else
       GPIO_WriteLow(RING_PORT, RING_PIN);
     }
-  }
-  else if((sys.flag_pm == 1) && check12h() == 1)
-  {
-          if(timenow.mn == 0)
-    {
-    if(timenow.ss < 4)
-    {
-      if(timenow.ss % 2 == 0)
-      {
-        GPIO_WriteHigh(RING_PORT, RING_PIN);
-      }
-      else
-        GPIO_WriteLow(RING_PORT, RING_PIN);
-    }
-    else
-      GPIO_WriteLow(RING_PORT, RING_PIN);
-    }
-  }
-  else
-    GPIO_WriteLow(RING_PORT, RING_PIN);
 }
 
 void System_Manager(void)
 {
    static uint8_t mode = 0;
+   static uint8_t hour;
    if(TIMER_CheckTimeMS(&sys.tick, 20)==0)
    {
      switch(sys.mode)
@@ -83,20 +59,6 @@ void System_Manager(void)
      case MODE_NORMAL:
        //modeget = keyget(MODE);
        led_set_blink(LED_BLINK_OFF);
-       if(timenow.hh / 10 == 0)
-       {
-         led_clear_digit(1);
-         led_set_digit(2, timenow.hh%10);
-         led_set_digit(3, timenow.mn/10);
-         led_set_digit(4, timenow.mn%10);
-       }
-       else
-       {
-         led_set_digit(1, timenow.hh/10);
-         led_set_digit(2, timenow.hh%10);
-         led_set_digit(3, timenow.mn/10);
-         led_set_digit(4, timenow.mn%10);
-       }
        system_ring();
        if(keyget(MODE) == KEY_PRESS)
        {
@@ -118,15 +80,35 @@ void System_Manager(void)
          if((sys.modeflag == 1) && (sys.modeReleaseTime != 0))
          {
            sys.flag_pm = !sys.flag_pm;
-           DS1307_SetPM(sys.flag_pm);
+//           DS1307_SetPM(sys.flag_pm);
            sys.modeflag = 0;
            sys.modePressTime = 0;
-           
          }
          else
          {
             sys.modeReleaseTime--;
          }
+       }
+        if(sys.flag_pm)
+       {
+         if(timenow.hh > 12)
+           hour = timenow.hh - 12;
+       }
+       else
+         hour = timenow.hh;
+       if(hour / 10 == 0)
+       {
+         led_clear_digit(1);
+         led_set_digit(2, hour%10);
+         led_set_digit(3, timenow.mn/10);
+         led_set_digit(4, timenow.mn%10);
+       }
+       else
+       {
+         led_set_digit(1, hour/10);
+         led_set_digit(2, hour%10);
+         led_set_digit(3, timenow.mn/10);
+         led_set_digit(4, timenow.mn%10);
        }
       // UpGet = keyget(UP);
        if(keyget(UP) == KEY_PRESS)
@@ -176,10 +158,20 @@ void System_Manager(void)
          case 1: 
             led_set_blink(LED_BLINK_2HZ);
             led_digit_blink(LED_12_BLINK);
-            led_set_digit(1, timeset.hh/10);
-            led_set_digit(2, timeset.hh%10);
-            led_set_digit(3, timeset.mn/10);
-            led_set_digit(4, timeset.mn%10);
+            if(timeset.hh / 10 == 0)
+            {
+              led_clear_digit(1);
+              led_set_digit(2, timeset.hh%10);
+              led_set_digit(3, timeset.mn/10);
+              led_set_digit(4, timeset.mn%10);
+            }
+            else
+            {
+              led_set_digit(1, timeset.hh/10);
+              led_set_digit(2, timeset.hh%10);
+              led_set_digit(3, timeset.mn/10);
+              led_set_digit(4, timeset.mn%10);
+            }
              if(keyget(UP) == KEY_PRESS)
              {
                ++sys.upPressTime;
